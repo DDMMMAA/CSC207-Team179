079873ae272b6a2f492bf4dd0ed48cb52a5c2de8
DMA <1670639177@qq.com>
2024-11-11 16:07:06 -0500

    add 'rankPointHistory' instance variable, 'getter', and 'addRankPointHistory'

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.util.ArrayList;
+
 /**
  * The representation of a password-protected user for our program.
  */
@@ -7,10 +9,13 @@ public class User {
 
     private final String name;
     private final String password;
+    private ArrayList<Integer> rankPointHistory;
 
     public User(String name, String password) {
         this.name = name;
         this.password = password;
+        this.rankPointHistory = new ArrayList<>();
+        this.rankPointHistory.add(0);
     }
 
     public String getName() {
@@ -19,6 +24,18 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public ArrayList<Integer> getRankPointHistory() {
+        return rankPointHistory;
+    }
+
+    /**
+     * This method add latest rankPoint to the end of rankPointHistory.
+     * @param rankPoint is the new rankPoint after one puzzle finished
+     */
+    public void addRankPointHistory(int rankPoint) {
+        this.rankPointHistory.add(rankPoint);
     }
 
 }
