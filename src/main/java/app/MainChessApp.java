b5917ecec7ebe5f25ade62b6074816cc120aa2d2
jiaqi ma <jiaqi.ma@mail.utoronto.ca>
2024-11-21 18:19:24 -0500

    all new file added in this commit comtain only minimal template (no logic), except 'MoveViewModel.java' & 'ChessBoardView.java' & 'ViewManager.java' which implemented to show a chessboard with pieces view. PS. default chessboard view can be seen by running 'ChessBoardViewTest.java'

---
+++ src/main/java/app/MainChessApp.java
@@ -0,0 +1,11 @@
+package app;
+
+import data_access.ChessDataAccessObject;
+import use_case.note.NoteDataAccessInterface;
+
+public class MainChessApp {
+
+    public static void main(String[] args) {
+
+    }
+}
