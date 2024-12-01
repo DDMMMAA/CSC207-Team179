b5bdb1fe0b02d9c541e76c7a90439c5a95381426
YannChi22 <yann.chi@mail.utoronto.ca>
2024-11-30 23:45:55 -0500

    Update Query view in interface adapter and create Query DAO file.

--- src/main/java/interface_adapter/query/QueryController.java
+++ src/main/java/interface_adapter/query/QueryController.java
@@ -1,4 +1,27 @@
 package interface_adapter.query;
 
+import use_case.query.QueryInputBoundary;
+import use_case.query.QueryInputData;
+
+/**
+ * The controller for the ShowProfile use case.
+ */
 public class QueryController {
+
+    private final QueryInputBoundary QueryInputInteractor;
+
+    public QueryController(QueryInputBoundary QueryInteractor) {
+        this.QueryInputInteractor = QueryInteractor;
+    }
+
+    /**
+     * Execute the showProfile use case.
+     * @param username the username of the user logging in
+     */
+    public void execute(String username) {
+        final QueryInputData queryInputData = new QueryInputData(username);
+
+        QueryInputInteractor.execute(queryInputData);
+    }
 }
+
--- src/main/java/interface_adapter/query/QueryPresenter.java
+++ src/main/java/interface_adapter/query/QueryPresenter.java
@@ -1,4 +1,35 @@
 package interface_adapter.query;
 
-public class QueryPresenter {
+import interface_adapter.ViewManagerModel;
+import use_case.query.QueryOutputBoundary;
+import use_case.query.QueryOutputData;
+import view.ViewManager;
+
+public class QueryPresenter implements QueryOutputBoundary {
+
+    private final QueryViewModel queryViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public QueryPresenter(QueryViewModel queryViewModel, ViewManagerModel viewManagerModel) {
+        this.queryViewModel = queryViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    @Override
+    public void prepareSuccessView(QueryOutputData outputData) {
+        // On success, switch to the Profile view.
+
+        final QueryState queryState = queryViewModel.getState();
+        queryState.setUsername(outputData.getUsername());
+        this.queryViewModel.setState(queryState);
+        this.queryViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setState(queryViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String errorMessage) {
+
+    }
 }
