7d53a34351d1002c87b8fb6db5f3ecf98adf480c
Chihana Kashiwabara <c.kashiwabara@mail.utoronto.ca>
2024-12-03 03:58:29 -0500

    Created UserAppBuilder and moved codes from MainChessApp to UserAppBuilder to follow the Clean Architecture

--- src/main/java/app/MainChessApp.java
+++ src/main/java/app/MainChessApp.java
@@ -1,24 +1,6 @@
 package app;
 
-import java.awt.CardLayout;
-
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
-
-import data_access.ChessDataAccessObject;
-import data_access.UserDataAccessObject;
-import interface_adapter.ViewManagerModel;
-import interface_adapter.logged_in.LoggedInViewModel;
-import interface_adapter.login.LoginViewModel;
-import interface_adapter.showProfile.ShowProfileViewModel;
-import interface_adapter.signup.SignupViewModel;
-import view.ChessBoardView;
-import view.LoggedInView;
-import view.LoginView;
-import view.ProfileView;
-import view.SignupView;
-import view.ViewManager;
 
 /**
  * Main that activates login page.
@@ -31,48 +13,12 @@ public class MainChessApp {
      */
     public static void main(String[] args) {
 
-        final JFrame frame = new JFrame("Chess Game Login");
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        final CardLayout cardLayout = new CardLayout();
-        final JPanel views = new JPanel(cardLayout);
-        frame.add(views);
-
-        final ViewManagerModel viewManagerModel = new ViewManagerModel();
-        new ViewManager(views, cardLayout, viewManagerModel);
-
-        final LoginViewModel loginViewModel = new LoginViewModel();
-        final LoggedInViewModel loggedInViewModel = new LoggedInViewModel();
-        final SignupViewModel signupViewModel = new SignupViewModel();
-        final ShowProfileViewModel showProfileViewModel = new ShowProfileViewModel();
-
-        final UserDataAccessObject userDataAccessObject = new UserDataAccessObject();
-
-        final SignupView signupView = SignupUseCaseFactory.create(viewManagerModel, loginViewModel,
-                signupViewModel, userDataAccessObject);
-        final LoginView loginView = LoginUseCaseFactory.create(viewManagerModel, loginViewModel,
-                loggedInViewModel, userDataAccessObject, signupViewModel);
-        final LoggedInView loggedInView = ShowProfileUseCaseFactory.create(viewManagerModel, loggedInViewModel,
-                signupViewModel, showProfileViewModel, userDataAccessObject);
-        final ProfileView profileView = new ProfileView(viewManagerModel, showProfileViewModel);
-
-        views.add(signupView, signupView.getViewName());
-        views.add(loginView, loginView.getViewName());
-        views.add(loggedInView, loggedInView.getViewName());
-        views.add(profileView, profileView.getViewName());
-
-        final ChessAppBuilder chessAppBuilder = new ChessAppBuilder();
-        chessAppBuilder.addChessDAO(new ChessDataAccessObject())
-                .addMoveView()
-                .addMoveUseCase();
-
-        final ChessBoardView chessBoardView = chessAppBuilder.buildChessBoardView();
-        views.add(chessBoardView, "chessBoardView");
-
-        viewManagerModel.setState(signupView.getViewName());
-        viewManagerModel.firePropertyChanged();
-
-        frame.pack();
-        frame.setVisible(true);
+        final UserAppBuilder userAppBuilder = new UserAppBuilder();
+        final JFrame application = userAppBuilder
+                .addViewModels()
+                .addViews()
+                .initializeState()
+                .addChessGame()
+                .build();
     }
 }
